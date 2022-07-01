779efd8370b04edec81aa2e412961197c1adaf43
Ryan Acevedo <racevedo@amplifire.com>
2022-06-30 19:59:20 -0600

    updated dashboard to render services

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,11 @@
+import { KucoinService } from './../services/kucoin.service';
 import { CoinbaseService } from './../services/coinbase.service';
 import { UpholdService } from './../services/uphold.service';
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs/internal/observable/forkJoin';
+import { CoinmetroService } from '../services/coinmetro.service';
+import { BinanceService } from '../services/binance.service';
+import { CryptoService } from '../services/crypto.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,16 +14,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   private currency = 'USD';
-  private blockchain = 'BTC';
+  private blockchain = 'ETH';
+  data = [];
   constructor(
     private upholdService: UpholdService,
-    private coinbaseService: CoinbaseService
-    ) { }
+    private coinbaseService: CoinbaseService,
+    private kucoinService: KucoinService,
+    private cryptoService: CryptoService,
+    private binanceService: BinanceService,
+    private coinmetroService: CoinmetroService
+  ) { }
 
   ngOnInit(): void {
-    this.coinbaseService.getPrices().subscribe(r => {
-
-    });
+    // forkJoin the service calls
+    forkJoin([
+      this.upholdService.getPrices(this.blockchain),
+      this.coinbaseService.getPrices(this.blockchain),
+      this.kucoinService.getPrices(this.blockchain),
+      this.binanceService.getPrices(this.blockchain),
+      this.coinmetroService.getPrices(this.blockchain),
+      this.cryptoService.getPrices(this.blockchain)
+    ]).subscribe(results => {
+      this.data = results;
+     }
+    );
   }
 
 }
